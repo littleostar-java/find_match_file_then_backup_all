23fce62bb00aa03be90fb3b032736d7cd10aaed4
littleOStar <qqduxingzhe@163.com>
2019-01-23 16:55:29 +0800

    update; update cmd update user variable; it's success;

---
+++ json_/update_user_variable_.bat
@@ -0,0 +1,8 @@
+
+setx CATALINA_HOME "C:\Tomcat 9.0";
+
+setx MAVEN_HOME "C:\apache-maven-3.6.0";
+
+setx SPRING_HOME "C:\spring-2.1.2.RELEASE";
+
+setx Path "C:\apache-maven-3.6.0\bin;C:\spring-2.1.2.RELEASE\bin;%Path%";
